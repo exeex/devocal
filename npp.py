81f85830081478f89c4ad3c9d3235483a5b537e9
cswu <xray0h@gmail.com>
2018-04-19 11:43:02 +0800

    add some qoute

--- npp.py
+++ npp.py
@@ -26,11 +26,22 @@ def pad_the_same(a,b):
     return a,b
 
 def right_shift(array,length):
+    """
+    :param array: origin signal
+    :param length: shift length
+    :return: shifted signal
+    """
+
     buf = right_pad(array, -length)
     return left_pad(buf,length)
 
 
 def left_shift(array, length):
+    """
+    :param array: origin signal
+    :param length: shift length
+    :return: shifted signal
+    """
     buf = left_pad(array, -length)
     return right_pad(buf, length)
 
@@ -39,12 +50,24 @@ def left_shift(array, length):
 
 def convolve(kernel_array, array):
 
+    """
+    convolve
+    :param kernel_array: kernel array
+    :param array: array to convolve
+    :return: left, right
+    left is a array of result of convolve along left side
+    left is a array of result of convolve along right side
+    """
 
+    #卷積
     convolved = np.convolve(kernel_array[::-1], array)
     print(convolved)
+
+    #往右shift的卷積
     right = convolved.copy()
     right = right[kernel_array.shape[0]-1:]
 
+    #往左shift的卷積
     left = convolved.copy()
     left = left[:kernel_array.shape[0]][::-1]
 
@@ -52,13 +75,17 @@ def convolve(kernel_array, array):
 
 
 def find_shift(kernel_array:np.ndarray, array:np.ndarray):
-    left, right = convolve(kernel_array, array)
 
+    #卷積
+    left, right = convolve(kernel_array, array)
     range = -1
 
+
+    #找到往右卷的最大值和位移
     r_max_idx = np.argmax(right[:range])
     r_max_value = right[:range].max()
 
+    #找到往左卷的最大值和位移
     l_max_idx = np.argmax(left[:range])
     l_max_value = left[:range].max()
 
@@ -66,6 +93,9 @@ def find_shift(kernel_array:np.ndarray, array:np.ndarray):
     print(l_max_idx,l_max_value)
 
     # return int(r_max_idx)
+    # 看往左往右哪個大，return那個值
+    # 往右是正數
+    # 往左是負數
     if r_max_value > l_max_value:
         return int(r_max_idx)
     else:
